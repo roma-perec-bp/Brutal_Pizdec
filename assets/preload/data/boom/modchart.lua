e98efd7e723f7b80a264328b517a98a4f01917be
Ender69 <112335251+ender69funk@users.noreply.github.com>
2024-05-06 23:04:29 +0500

    Вот кому-то делать нехуй

---
+++ assets/preload/data/boom/modchart.lua
@@ -0,0 +1,40 @@
+
+function onCreate()
+    makeLuaSprite('maniaPart', null, 360, 0)
+	makeGraphic('maniaPart', 560, 720, '000000')
+	setObjectCamera('maniaPart', 'hud')
+	setProperty('maniaPart.alpha', 0.00001)
+	addLuaSprite('maniaPart', false)
+	
+    --начало модчарта хахаха
+    makeLuaSprite('blackFlash', null, 0, 0)
+    makeGraphic('blackFlash', 1280, 720, '000000')
+    setObjectCamera('blackFlash', 'hud')
+    addLuaSprite('blackFlash', false)
+    
+    makeLuaSprite('vin', 'vin', 0, 0)
+    setObjectCamera('vin', 'hud')
+    addLuaSprite('vin', true)
+end
+
+function onStepHit()
+    if curStep == 32 then
+        setProperty('blackFlash.visible', false)
+    end
+    if curStep == 288 then
+        setProperty('vin.visible', false)
+    end
+    
+    if curStep == 2720 then
+        doTweenAlpha('maniaPartYea','maniaPart', 1, 0.1, 'linear')
+    end
+    if curStep == 2971 then
+        doTweenAlpha('Fucked','maniaPart', 0, 0.1, 'linear')
+    end
+    if curStep == 3072 then
+        setProperty('vin.visible', true)
+    end
+    if curStep == 3854 then
+        setProperty('vin.visible', false)
+    end
+end
