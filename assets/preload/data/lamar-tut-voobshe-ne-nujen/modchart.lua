417596a3505d7b30b3bf9db20494a9e0e7ee67a0
Ender69 <112335251+ender69funk@users.noreply.github.com>
2024-05-08 23:53:21 +0500

    Ура ивенты

---
+++ assets/preload/data/lamar-tut-voobshe-ne-nujen/modchart.lua
@@ -0,0 +1,35 @@
+local angleshit = 2;
+local anglevar = 2;
+
+function onBeatHit()
+	if curBeat >= 32 and curBeat < 96 then
+		if curBeat % 2 == 0 then
+			angleshit = anglevar;
+		else
+			angleshit = -anglevar;
+		end
+		setProperty('camHUD.angle',angleshit*3)
+		setProperty('camGame.angle',angleshit*3)
+		doTweenAngle('turn', 'camHUD', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('tuin', 'camHUD', -angleshit*8, crochet*0.001, 'linear')
+		doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
+	elseif curBeat >= 192 and curBeat < 256 then
+		if curBeat % 2 == 0 then
+			angleshit = anglevar;
+		else
+			angleshit = -anglevar;
+		end
+		setProperty('camHUD.angle',angleshit*3)
+		setProperty('camGame.angle',angleshit*3)
+		doTweenAngle('turn', 'camHUD', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('tuin', 'camHUD', -angleshit*8, crochet*0.001, 'linear')
+		doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
+	else
+		setProperty('camHUD.angle',0)
+		setProperty('camHUD.x',0)
+		setProperty('camHUD.x',0)
+	end
+		
+end
