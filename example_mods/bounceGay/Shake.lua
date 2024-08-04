e8c929ac8663370c330650bac52cf1f340b19a31
Rom4chek <97591537+Rom4chek@users.noreply.github.com>
2024-08-04 23:48:33 +0500

    в пезду

--- example_mods/bounceGay/Shake.lua
+++
@@ -1,7 +0,0 @@
-function onUpdatePost()
-    setProperty('iconP1.offset.x', getRandomFloat(-2.5, 2.5))
-    setProperty('iconP1.offset.y', getRandomFloat(-2.5, 2.5))
-    
-    setProperty('iconP2.offset.x', getRandomFloat(-2.5, 2.5))
-    setProperty('iconP2.offset.y', getRandomFloat(-2.5, 2.5))
-end
