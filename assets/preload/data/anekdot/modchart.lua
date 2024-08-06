eaa1072e0b1b3066cb58cc191325e3b67c520468
Ender69 <112335251+ender69funk@users.noreply.github.com>
2024-08-06 10:25:33 +0500

    С твином

--- assets/preload/data/anekdot/modchart.lua
+++ assets/preload/data/anekdot/modchart.lua
@@ -2,25 +2,11 @@ local angleshit = 0.5;
 local anglevar = 0.5;
 
 function onBeatHit()
-	if curBeat >= 224 and curBeat < 288 then
+	if curBeat >= 224 and curBeat < 288 and curBeat >= 384 and curBeat < 448 then
 		triggerEvent('Add Camera Zoom', 0.04,0.05)
 
 		if curBeat % 2 == 0 then
-			angleshit = anglevar;
-		else
-			angleshit = -anglevar;
-		end
-		setProperty('camHUD.angle',angleshit*3)
-		setProperty('camGame.angle',angleshit*3)
-		doTweenAngle('turn', 'camHUD', angleshit, stepCrochet*0.002, 'circOut')
-		doTweenX('tuin', 'camHUD', -angleshit*8, crochet*0.001, 'linear')
-		doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
-		doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
-	elseif curBeat >= 384 and curBeat < 448 then
-	    triggerEvent('Add Camera Zoom', 0.04,0.05)
-
-		if curBeat % 2 == 0 then
-			angleshit = anglevar;
+		        angleshit = anglevar;
 		else
 			angleshit = -anglevar;
 		end
@@ -31,9 +17,10 @@ function onBeatHit()
 		doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
 		doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
 	else
-		setProperty('camHUD.angle',0)
-		setProperty('camHUD.x',0)
-		setProperty('camHUD.x',0)
+		doTweenAngle('dick', 'camHUD', 0, stepCrochet * 0.005, 'circOut')
+                doTweenAngle('boobs', 'camGame', 0, stepCrochet * 0.005, 'circOut')
+	        doTweenX('dickB', 'camHUD', 0, stepCrochet * 0.005, 'circOut')
+                doTweenX('boobsB', 'camGame', 0, stepCrochet * 0.005, 'circOut')
 	end
 		
 end
