49184b1100ff053df46651348c44bfc48abee103
Rom4chek <97591537+Rom4chek@users.noreply.github.com>
2024-08-09 13:43:19 +0500

    Update StoryMenuState.hx

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -275,10 +275,6 @@ class StoryMenuState extends MusicBeatState
 				LoadingState.loadAndSwitchState(new PlayState(), true);
 				FreeplayState.destroyFreeplayVocals();
 			});
-			
-			#if MODS_ALLOWED
-			DiscordClient.loadModRPC();
-			#end
 		} else {
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 		}
