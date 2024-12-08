39948a1221168a41749a46f4f721cc86bde64202
Ender69 <112335251+ender69funk@users.noreply.github.com>
2024-12-08 18:41:15 +0500

    Update PauseSubState.hx

--- source/substates/PauseSubState.hx
+++ source/substates/PauseSubState.hx
@@ -190,7 +190,7 @@ class PauseSubState extends MusicBeatSubstate
 						PlayState.instance.vocals.volume = 0;
 						MusicBeatState.switchState(new OptionsState());
 
-						FlxG.sound.playMusic(Paths.music(pizdec), pauseMusic.volume);
+						FlxG.sound.playMusic(Paths.music('pizdec'), pauseMusic.volume);
 						FlxTween.tween(FlxG.sound.music, {volume: 1}, 0.8);
 						FlxG.sound.music.time = pauseMusic.time;
 
